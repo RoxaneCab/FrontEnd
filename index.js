844533e74539f21245138cfa9350fd141e34c115
Roxane Cabit <roxane.cabit@gmail.com>
2023-11-03 10:38:21 +0100

    createWorks

--- index.js
+++ index.js
@@ -1,17 +1,94 @@
 
+// Récupérer les "works" et les afficher
 
-// function createArticles() {
+const url = 'http://localhost:5678/api/works' 
+const container = document.getElementsByClassName("gallery")
 
-//     fetch('http://localhost:5678/api/works'), {
-//         headers: {
-//             method: "GET"
-//         }
-//     }.then((response) => {
-//         console.log(response)
+function createWorks() {
+    fetch(url)
+    .then(function (res) {
+        return res.json()
+    })
+    .then(function (works) {
+
+        works.forEach(element => {
+
+            let figure = document.createElement('figure')
+
+            let figcaption = document.createElement('figcaption')
+            figcaption.innerHTML = element.title
+
+            let img = document.createElement('img')
+            img.src = element.imageUrl
+
+            figure.appendChild(img)
+            figure.appendChild(figcaption)
+
+            let gallery = document.querySelector('.gallery')
+
+            gallery.appendChild(figure)
+        });
+        
+    })
+}
+
+createWorks()
+
+
+
+
+fetch('http://localhost:5678/api/categories')
+.then(response => response.json())
+.then(categories => {
+    const filterButton = document.querySelector('.filter-buttons');
+})
+
+// map pour structure html de chaque cat
+const buttonHtml = categories.map(category => {
+    return
+    `<button class="filter">${category.name}</button>;`
+}).join('')
+
+//  Bouton "Tous" en 1er
+const allButtonHtml = `<button class="filter filter-selected>Tous</button> `
+
+// Crée la structure de tous les boutons (Tous + autre cat plus haut)
+const filterButtonHtml = allButtonHtml + buttonHtml
+
+
+
+//   // Catégories TRY 1
+
+//   const urlCategories = 'http://localhost:5678/api/categories' 
+
+//   function fetchCategories() {
+//   fetch(urlCategories)
+//     .then(function (res) {
+//         return res.json()
+//     })
+// }
+
+// fetchCategories()
+
+//     // Boutons
+
+//     const btnTous = document.querySelector(".bton-tous")
+//     btnTous.addEventListener("click", function() {
+//        displayProjects(data)
 //     })
 
-// }           
+//     const btnObjets = document.querySelector(".bton-objets")
+//     btnTous.addEventListener("click", function() {
+//         displayProjects(data)
+//      })
 
-const articles = fetch('http://localhost:5678/api/works');
-console.log(articles)
+//     const btnAppart = document.querySelector(".bton-appart")
+//     btnTous.addEventListener("click", function() {
+//         displayProjects(data)
+//      }) 
+
+//     const btnHotel = document.querySelector(".bton-hotel")
+//         btnTous.addEventListener("click", function() {
+//        displayProjects(data)
+//     })
 
