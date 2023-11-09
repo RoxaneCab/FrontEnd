3fa83101467bab72733cccb9fed593ded6ba5549
Roxane Cabit <roxane.cabit@gmail.com>
2023-11-09 11:29:09 +0100

    Filter event first try

--- index.js
+++ index.js
@@ -1,68 +1,135 @@
 
-// Récupérer les "works" et les afficher
+/* 1_ RETRIEVE WORKS IN BACK, AND DISPLAY THEM */
 
-const url = 'http://localhost:5678/api/works' 
+const url = 'http://localhost:5678/api/works'
 const container = document.getElementsByClassName("gallery")
 
 function createWorks() {
     fetch(url)
-    .then(function (res) {
-        return res.json()
-    })
-    .then(function (works) {
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (works) {
 
-        works.forEach(element => {
+            works.forEach(element => {
 
-            let figure = document.createElement('figure')
+                let figure = document.createElement('figure')
 
-            let figcaption = document.createElement('figcaption')
-            figcaption.innerHTML = element.title
+                let figcaption = document.createElement('figcaption')
+                figcaption.innerHTML = element.title
 
-            let img = document.createElement('img')
-            img.src = element.imageUrl
+                let img = document.createElement('img')
+                img.src = element.imageUrl
 
-            figure.appendChild(img)
-            figure.appendChild(figcaption)
+                figure.appendChild(img)
+                figure.appendChild(figcaption)
 
-            let gallery = document.querySelector('.gallery')
+                let gallery = document.querySelector('.gallery')
 
-            gallery.appendChild(figure)
-        });
-        
-    })
+                gallery.appendChild(figure)
+
+                const id = element.categoryId;
+                console.log(id)
+            });
+
+        })
 }
 
 createWorks()
 
-
-
+/* 2_ BUTTONS CREATION */
 
 fetch('http://localhost:5678/api/categories')
-.then(response => response.json())
-.then(categories => {
-    const filterButton = document.querySelector('.filter-buttons');
-})
+    .then(response => response.json())
+    .then(categories => {
 
-// map pour structure html de chaque cat
-const buttonHtml = categories.map(category => {
-    return
-    `<button class="filter">${category.name}</button>;`
-}).join('')
+        const filterButton = document.querySelector('.filter-button');
+        console.log(categories)
 
-//  Bouton "Tous" en 1er
-const allButtonHtml = `<button class="filter filter-selected>Tous</button> `
+        let tousButton = document.createElement('button')
+        tousButton.innerHTML = "Tous"
+        filterButton.appendChild(tousButton)
 
-// Crée la structure de tous les boutons (Tous + autre cat plus haut)
-const filterButtonHtml = allButtonHtml + buttonHtml
+        categories.forEach((category) => {
+            let button = document.createElement('button')
+            button.innerHTML = category.name
+            filterButton.appendChild(button)
 
-// innerHTML pour mettre à jour le contenu de la div filter-buttons
-filterButton.innerHTML =
+            // association
+
+            button.dataset.id = category.id
+
+            button.addEventListener('click', (event) => {
+                console.log(event.target)
+                // categoryIdValue = category.name;
+                // console.log(categoryIdValue);
+                // if (categoryIdValue = id)
+                // if (category.id === id) {
+
+                // }
+
+                //     const btn1 = document.querySelector(".bton")
+                //     btnTous.addEventListener("click", function() {
+                //        displayProjects(data)
+                //     })
+
+                //     const btn2 = document.querySelector(".bton")
+                //     btnTous.addEventListener("click", function() {
+                //         displayProjects(data)
+                //      })
+
+                //     const btn3 = document.querySelector(".bton")
+                //     btnTous.addEventListener("click", function() {
+                //         displayProjects(data)
+                //      }) 
+
+                //     const btn4 = document.querySelector(".bton")
+                //         btnTous.addEventListener("click", function() {
+                //        displayProjects(data)
+                //     })
 
 
 
-//   // Catégories TRY 1
 
-//   const urlCategories = 'http://localhost:5678/api/categories' 
+            })
+        })
+
+    })
+
+
+
+
+
+
+/* BUTTONS, old ideas */
+
+/* OLD ONE, WORKS BUT IS NOT OPTIMAL */
+
+// fetch('http://localhost:5678/api/categories')
+// .then(response => response.json())
+// .then(categories => {
+//     const filterButton = document.querySelector('.filter-buttons');
+// })
+
+// // map pour structure html de chaque cat
+// const buttonHtml = categories.map(category => {
+//     return `<button class="filter">${category.name}</button>;`
+// }).join('')
+
+// //  Bouton "Tous" en 1er
+// const allButtonHtml = `<button class="filter filter-selected>Tous</button> `
+
+// // Crée la structure de tous les boutons (Tous + autre cat plus haut)
+// const filterButtonHtml = allButtonHtml + buttonHtml
+
+// // innerHTML pour mettre à jour le contenu de la div filter-buttons
+// filterButton.innerHTML = filterButtonHtml
+
+// // Recupere tous les boutons de filtres
+// const buttons = document.querySelectorAll('.filter-buttons button')
+
+/* FIRST TRY 1 AND IDEAS */
+//   const urlCategories = 'http://localhost:5678/api/categories'
 
 //   function fetchCategories() {
 //   fetch(urlCategories)
@@ -75,23 +142,5 @@ filterButton.innerHTML =
 
 //     // Boutons
 
-//     const btnTous = document.querySelector(".bton-tous")
-//     btnTous.addEventListener("click", function() {
-//        displayProjects(data)
-//     })
 
-//     const btnObjets = document.querySelector(".bton-objets")
-//     btnTous.addEventListener("click", function() {
-//         displayProjects(data)
-//      })
-
-//     const btnAppart = document.querySelector(".bton-appart")
-//     btnTous.addEventListener("click", function() {
-//         displayProjects(data)
-//      }) 
-
-//     const btnHotel = document.querySelector(".bton-hotel")
-//         btnTous.addEventListener("click", function() {
-//        displayProjects(data)
-//     })
 
